d88865cdf5f42a41f122dfda2f50dc7ad1bc923a
ilovepitsa <serialch1ller1537@gmail.com>
2024-03-27 21:29:25 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+//try 1
+
+func IsAnnagrans() {
+
+}
+
+func groupAnnagrams(wordsList []string) [][]string {
+	result := [][]string{}
+	for _, word := range wordsList {
+
+	}
+	return result
+}
+
+func main() {
+	words := []string{"материк", "клоун", "метрика", "уклон", "Керамит", "норка", "колун", "рок"}
+	fmt.Println(groupAnnagrams(words))
+}
